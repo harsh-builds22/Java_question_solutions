c0a8839a37af8c280120fd97889560f9170fad05
Harsh Sharma <harshsh2214@gmail.com>
2026-06-22 19:33:03 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.16%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,42 +1,29 @@
-import java.math.BigInteger;
 class Solution {
-    public int[] plusOne(int[] arr){
-       String number = sum1(arr);
-
-        int[] arr1 = new int[number.length()];
-
-       for (int i = 0; i < arr1.length; i++) {
-           arr1[i] = number.charAt(i) - '0';
-       }
-
-       return arr1;
-
-   }
-
-
-
-
-
-
-
-
-    public String array(int[] arr){
-        String num = "";
-        for (int i = 0; i < arr.length; i++) {
-             num = num + arr[i];
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int carry = 1; // Shuru me hume 1 plus karna hai, isliye carry = 1
+        
+        // Right se Left ki taraf loop chalayenge
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = digits[i] + carry; // Current digit aur carry ko jodo
+            
+            digits[i] = sum % 10;        // Agar sum 10 hua to ye 0 banayega, nahi to vahi number rahega
+            carry = sum / 10;            // Agar sum 10 hua to carry 1 banega, nahi to 0
+            
+            // Agar kisi point par carry 0 ho gaya, to aage loop chalane ki zaroorat hi nahi!
+            if (carry == 0) {
+                return digits;
+            }
         }
-
-        return num;
+        
+        // EDGE CASE: Agar loop khatam hone ke baad bhi carry 1 bacha hai (jaise 999 + 1 me)
+        if (carry == 1) {
+            int[] ans = new int[n + 1];
+            ans[0] = 1; // Sabse aage 1 rakh do
+            // Baaki saare elements naye array me by default 0 hi hote hain
+            return ans;
+        }
+        
+        return digits;
     }
-
-
-
-    public String sum1(int[] arr){
-         String sum1 = array(arr);         // return string
-        BigInteger num1 = new BigInteger(sum1);      // for big number, parseInt  100-digit number yahan error deta!  [convert to integer]
-        num1 = num1.add(BigInteger.ONE);
-          sum1 = num1.toString();  // convert to string
-        return sum1;
-    }
-
 }
