2293c8ddbd4dfe5ec69ee423459677de7146205a
Harsh Sharma <harshsh2214@gmail.com>
2026-08-10 00:42:03 +0530

    Time: 1 ms (89.75%), Space: 42.7 MB (33.81%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        if ((n & (n - 1)) == 0) {
+            int odd = countEven(n);
+            if (odd % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int countEven(int n) {
+        int count = 1;
+        while (n > 0) {
+            int last = n & 1;
+            n = n >> 1;
+            if (last == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
