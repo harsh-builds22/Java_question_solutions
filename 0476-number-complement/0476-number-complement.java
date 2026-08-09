19fdb35911aa73e58388c861f2b514b58680e736
Harsh Sharma <harshsh2214@gmail.com>
2026-08-09 15:11:55 +0530

    Time: 0 ms (100%), Space: 42 MB (74.64%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findComplement(int n) {
+        int count = bitLength(n);
+        int mask = (1 << count) - 1;
+
+        int num = n ^ mask;
+        return num;
+
+    }
+
+    public int bitLength(int n) {
+
+        int count = 0;
+        while (n > 0) {
+            n = n >> 1;
+            count++;
+        }
+        return count;
+    }
+
+}
