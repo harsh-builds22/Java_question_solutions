44cc68bbafeb946a46ac5ecb8bf7c9eb6c87d289
Harsh Sharma <harshsh2214@gmail.com>
2026-08-14 14:37:07 +0530

    Time: 581 ms (5.04%), Space: 53.4 MB (72.23%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,27 +1,26 @@
 class Solution {
     public int equalPairs(int[][] grid) {
+
         int count = 0;
-
+        //  ye loop sari rows tak jayega
         for (int i = 0; i < grid.length; i++) {
-
-            List<Integer> rowlist = new ArrayList<>();
-            for (int j = 0; j < grid[i].length; j++) {
-                rowlist.add(grid[i][j]);
+            // isme individual rows ke elements save honge.
+            List<Integer> rowList = new ArrayList<>();
+            // isse sare rows ke elements save honge bcz n*n ka matrix he isliye grid.length aya
+            for (int j = 0; j < grid.length; j++) {
+                rowList.add(grid[i][j]);
             }
 
+            // abb ek loop jisse columns check karnge add karte karte
             for (int c = 0; c < grid.length; c++) {
                 List<Integer> collist = new ArrayList<>();
                 for (int k = 0; k < grid.length; k++) {
                     collist.add(grid[k][c]);
                 }
-
-                if (rowlist.equals(collist)) {
+                if (rowList.equals(collist)) {
                     count++;
-
                 }
-
             }
-
         }
         return count;
     }
