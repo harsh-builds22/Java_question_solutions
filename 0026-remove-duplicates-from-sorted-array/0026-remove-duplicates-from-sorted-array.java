5fae6bc4807dca8baf2c65ef6c24971290965721
Harsh Sharma <harshsh2214@gmail.com>
2026-09-02 21:08:01 +0530

    Time: 1 ms (77.57%), Space: 46.9 MB (24.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,14 +3,14 @@ class Solution {
 public int removeDuplicates(int[] arr) {
     if (arr.length == 0) return 0;
 
-    int m = 1;  // index 0 already sahi jagah hai, agla unique yaha se store hoga
+    int n = 1;  // index 0 already sahi jagah hai, agla unique yaha se store hoga
     for (int i = 1; i < arr.length; i++) {
         if (arr[i] != arr[i - 1]) {
-            arr[m] = arr[i];
-            m++;
+            arr[n] = arr[i];
+            n++;
         }
     }
-    return m;   // k hi represent karta hai kitne unique elements store hue
+    return n;   // k hi represent karta hai kitne unique elements store hue
 }
      
 
